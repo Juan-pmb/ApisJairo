9609e22b4d08f02e90e28ab726cc53ef61fb561b
Juan-pmb <papomunoz06@gmail.com>
2024-04-08 13:25:40 -0500

    Se agregó webApi1

---
+++ WebApplication1/WebApplication1/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public DbSet<Product> Product {  get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }    
+
+        public int Precio { get; set; }    
+
+        public int Stock { get; set; }      
+    }
+}
